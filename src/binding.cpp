35fdc6c2a457e1082b096b2872d5710d65e3d57f
David Nixon <david_nixon@us.ibm.com>
2021-11-02 16:08:25 -0400

    test format error

--- src/binding.cpp
+++ src/binding.cpp
@@ -1,7 +1,8 @@
 #include "sdlgamecontroller.h"
 #include <napi.h>  // NOLINT
 
-Napi::Object Init(Napi::Env env, Napi::Object exports) {
+Napi::Object Init(Napi::Env env, Napi::Object exports) 
+{
   SdlGameController::Init(env, exports);
   return exports;
 }
