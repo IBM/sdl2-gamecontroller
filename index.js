5ba9d89d377ede042d843269e4ee13815ce5e58e
David Nixon <davidnixon@users.noreply.github.com>
2022-03-29 14:13:44 -0500

    Fix add eslint (#10)
    * fix: add eslint and resolve all errors found. Also add eslint and cppcheck to CI workflow.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-///<reference path="index.d.ts" />
+// /<reference path="index.d.ts" />
 
 const EventEmitter = require('events').EventEmitter;
 const SdlGameController =
--- test/helloworld.ts
+++ test/helloworld.ts
@@ -3,8 +3,8 @@ import gamecontroller from '../index';
 gamecontroller.on('error', (data) => console.log('error', data));
 gamecontroller.on('warning', (data) => console.log('warning', data));
 gamecontroller.on('sdl-init', (data) => console.log('SDL2 Initialized', data));
-gamecontroller.on('a:down', (data) => console.log('Hello A button world'));
+gamecontroller.on('a:down', () => console.log('Hello A button world'));
 gamecontroller.on('controller-device-added', (data) =>
   console.log('controller connected', data.name),
 );
-gamecontroller.on('x:down', (data) => process.exit(0));
+gamecontroller.on('x:down', () => process.exit(0));
